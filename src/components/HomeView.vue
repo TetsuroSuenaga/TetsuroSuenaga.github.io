6617fc3da4edcafbe8294cdbb0f14a403424b988
TetsuroSuenaga <you@exanple.com>
2023-09-22 15:10:00 +0900

    init

---
+++ src/components/HomeView.vue
@@ -0,0 +1,27 @@
+<template>
+	<div>
+		<div class="text-center m-5">
+			<img alt="logo" src="../assets/icon.jpg" class="m-auto w-80 rounded-3xl" loading=”lazy”>	
+			<p class="text-2xl font-momo">Tetsuro Suenaga</p>
+			<p class="text-lg font-momo text-slate-500"></p>大学生 学部4年</div>
+
+		<div class="w-60 mx-auto my-10 flex flex-col gap-4">
+			<button class="sns-btn" type="button" onclick="location.href='https://github.com/TetsuroSuenaga/'">
+				<img src="../assets/github.svg" alt="GitHub icon" class="w-10" loading=”lazy”>
+				<span class="mx-auto">GitHub</span>
+			</button>
+		</div>
+		
+		<div>
+			<p class="headline">About me</p>
+			<p>
+				情報系。<br>
+				現在、スマート農業ロボットの研究を行っていて
+				<span class="md:inline block">
+				専門分野はCV。
+				</span>
+				<br>
+			</p>
+		</div>
+	</div>
+</template>
